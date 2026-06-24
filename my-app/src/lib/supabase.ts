230a2f1c78918b0614bfd91655ac658af49ac4b6
Daiki Iha <daiki-iha-k24c@stu.kbc.ac.jp>
2026-06-24 11:29:22 +0900

    キーを.envに分離

--- my-app/src/lib/supabase.ts
+++ my-app/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kfhgwyqofgnmzpgmedrk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmaGd3eXFvZmdubXpwZ21lZHJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NjYzMTMsImV4cCI6MjA5NzA0MjMxM30.POYd9yV319Zn1m-DwGaBvGijgXdSokjooBa_2ii1KxE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
